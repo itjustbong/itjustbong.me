660243c933da5ea55c162d263feaa76be88ad637
Bong <qhdgkdbs@gmail.com>
2023-01-28 06:05:04 +0900

    💬 모바일 환경에서 SNS 버튼 추가

--- src/components/Layout/cards/ContactCard.tsx
+++ src/components/Layout/cards/ContactCard.tsx
@@ -12,50 +12,45 @@ const ContactCard: React.FC = () => {
     <>
       <div className="p-1 mb-3 dark:text-white">💬 Contact</div>
       <ul className="rounded-md bg-white dark:bg-zinc-700 p-1 flex flex-col">
-        {CONFIG.profile.github && (
-          <a
-            href={`https://github.com/${CONFIG.profile.github}`}
-            rel="noreferrer"
-            target="_blank"
-            className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
-          >
-            <AiOutlineGithub className="text-2xl" />
-            <div className="text-sm">github</div>
-          </a>
-        )}
-        {CONFIG.profile.instagram && (
-          <a
-            href={`https://www.instagram.com/${CONFIG.profile.instagram}`}
-            rel="noreferrer"
-            target="_blank"
-            className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-          >
-            <AiOutlineInstagram className="text-2xl" />
-            <div className="text-sm">instagram</div>
-          </a>
-        )}
-        {CONFIG.profile.email && (
-          <a
-            href={`mailto:${CONFIG.profile.email}`}
-            rel="noreferrer"
-            target="_blank"
-            className="overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-          >
-            <AiOutlineMail className="text-2xl flex-shrink-0" />
-            <div className="text-sm">email</div>
-          </a>
-        )}
-        {CONFIG.profile.linkedin && (
-          <a
-            href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
-            rel="noreferrer"
-            target="_blank"
-            className="  overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
-          >
-            <AiFillLinkedin className="text-2xl flex-shrink-0" />
-            <div className="text-sm">linkedin</div>
-          </a>
-        )}
+        <a
+          href={`https://github.com/${CONFIG.profile.github}`}
+          rel="noreferrer"
+          target="_blank"
+          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
+        >
+          <AiOutlineGithub className="text-2xl" />
+          <div className="text-sm">github</div>
+        </a>
+
+        <a
+          href={`https://www.instagram.com/${CONFIG.profile.instagram}`}
+          rel="noreferrer"
+          target="_blank"
+          className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+        >
+          <AiOutlineInstagram className="text-2xl" />
+          <div className="text-sm">instagram</div>
+        </a>
+
+        <a
+          href={`mailto:${CONFIG.profile.email}`}
+          rel="noreferrer"
+          target="_blank"
+          className="overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+        >
+          <AiOutlineMail className="text-2xl flex-shrink-0" />
+          <div className="text-sm">email</div>
+        </a>
+
+        <a
+          href={`https://www.linkedin.com/in/${CONFIG.profile.linkedin}`}
+          rel="noreferrer"
+          target="_blank"
+          className="  overflow-hidden p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-md cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white"
+        >
+          <AiFillLinkedin className="text-2xl flex-shrink-0" />
+          <div className="text-sm">linkedin</div>
+        </a>
       </ul>
     </>
   )
