09a846de800f16f33aee81ca02c39cc0ddbbe68b
Bong <qhdgkdbs@gmail.com>
2023-01-28 05:07:42 +0900

    🔧 utterances 정보 업데이트

--- site.config.js
+++ site.config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "itjustbong/itjustbong.me",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
