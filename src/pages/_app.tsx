0f118d9e99d006a6df66bd07176b318446ef5af8
Bong <qhdgkdbs@gmail.com>
2023-04-22 17:15:06 +0900

    Feat: @vercel/analytics 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
-import 'prismjs/themes/prism.css'
-import 'react-notion-x/src/styles.css'
-import 'katex/dist/katex.min.css'
-import '@styles/globals.css'
-import '@styles/notion.css'
-import useThemeEffect from '@hooks/useThemeEffect'
-import useGtagEffect from '@hooks/useGtagEffect'
-import Scripts from '@components/Scripts'
-import { NextPage } from 'next'
-import { ReactElement, ReactNode } from 'react'
-import { AppProps } from 'next/app'
+import "prismjs/themes/prism.css"
+import "react-notion-x/src/styles.css"
+import "katex/dist/katex.min.css"
+import "@styles/globals.css"
+import "@styles/notion.css"
+import useThemeEffect from "@hooks/useThemeEffect"
+import useGtagEffect from "@hooks/useGtagEffect"
+import Scripts from "@components/Scripts"
+import { NextPage } from "next"
+import { ReactElement, ReactNode } from "react"
+import { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 
 export type NextPageWithLayout<PageProps = {}> = NextPage<PageProps> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -25,6 +26,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
+      <Analytics />
       <Scripts />
       {getLayout(<Component {...pageProps} />)}
     </>
