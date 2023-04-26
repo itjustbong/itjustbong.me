45c72c2370368e07ee02ac5bc624b9e64776d99e
Bong <qhdgkdbs@gmail.com>
2023-04-27 00:42:30 +0900

    Style: 헤더 스타일 변경

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -13,7 +13,7 @@ const Header: React.FC<Props> = ({ fullWidth }) => {
 
   return (
     <div
-      className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-6 py-8 bg-opacity-60 max-w-6xl px-4 ${
+      className={`mt-2 top-2 my-2 bg-white sticky shadow-sm rounded-2xl sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-6 py-8 bg-opacity-60 max-w-6xl px-4 ${
         fullWidth && "px-4 md:px-24"
       }`}
       id="sticky-nav"
