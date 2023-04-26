e857ca73a8798cca0c7f977b1db2f338bfc911d0
Bong <qhdgkdbs@gmail.com>
2023-04-27 01:02:48 +0900

    Style: 포스트 배열 그리드 칼럼 2로 수정

--- src/containers/Feed/components/lists/PostList.tsx
+++ src/containers/Feed/components/lists/PostList.tsx
@@ -1,7 +1,7 @@
-import PostCard from '@components/PostCard'
-import { TPosts, TTags } from '@/src/types'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import PostCard from "@components/PostCard"
+import { TPosts, TTags } from "@/src/types"
+import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react"
 
 type Props = {
   q: string
@@ -13,27 +13,27 @@ const PostList: React.FC<Props> = ({ q, posts, tags }) => {
   const router = useRouter()
   const [filteredPosts, setFilteredPosts] = useState(posts)
 
-  const currentTag = `${router.query.tag || ``}` || 'All'
-  const currentOrder = `${router.query.order || ``}` || 'desc'
+  const currentTag = `${router.query.tag || ``}` || "All"
+  const currentOrder = `${router.query.order || ``}` || "desc"
 
   useEffect(() => {
     setFilteredPosts(() => {
       let filteredPosts = posts
       // keyword
       filteredPosts = filteredPosts.filter((post) => {
-        const tagContent = post.tags ? post.tags.join(' ') : ''
+        const tagContent = post.tags ? post.tags.join(" ") : ""
         const searchContent = post.title + post.summary + tagContent
         return searchContent.toLowerCase().includes(q.toLowerCase())
       })
 
       // tag
-      if (currentTag !== 'All') {
+      if (currentTag !== "All") {
         filteredPosts = filteredPosts.filter(
           (post) => post && post.tags && post.tags.includes(currentTag)
         )
       }
       // order
-      if (currentOrder !== 'desc') {
+      if (currentOrder !== "desc") {
         filteredPosts = filteredPosts.reverse()
       }
 
@@ -43,11 +43,11 @@ const PostList: React.FC<Props> = ({ q, posts, tags }) => {
 
   return (
     <>
-      <div className="my-2">
+      <div className="lg:grid grid-cols-2 gap-2">
         {!filteredPosts.length && (
           <p className="text-gray-500 dark:text-gray-300">Nothing! 😺</p>
         )}
-        {filteredPosts.slice(0, 20).map((post) => (
+        {filteredPosts.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
       </div>
