9a7de51c09571199019509f33cb278b4f9d93059
Bong <qhdgkdbs@gmail.com>
2023-05-03 01:03:16 +0900

    Feat: getHueImage 추가"

--- src/libs/utils/image.ts
+++ src/libs/utils/image.ts
@@ -1,9 +1,8 @@
-const cache: any = {}
-
-export function getHueColorOfImage(imgElement: HTMLImageElement) {
-  if (cache.hasOwnProperty(imgElement.src)) {
-    return cache[imgElement.src]
-  }
+export function getHueColorOfImage(
+  imgSrc: string,
+  width: number,
+  height: number
+) {
   const canvas = document.createElement("canvas")
   const context = canvas.getContext && canvas.getContext("2d")
   const averageColor = {
@@ -16,28 +15,30 @@ export function getHueColorOfImage(imgElement: HTMLImageElement) {
     return averageColor
   }
 
-  const width = (canvas.width =
-    imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width)
-  const height = (canvas.height =
-    imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height)
+  return new Promise((resolve, reject) => {
+    try {
+      let image = new Image()
+      image.src = imgSrc
+      image.onload = () => {
+        context.drawImage(image, 0, 0)
+        const imageData = context.getImageData(0, 0, width, height).data
+        const length = imageData.length
 
-  context.drawImage(imgElement, 0, 0)
+        for (let i = 0; i < length; i += 4) {
+          averageColor.r += imageData[i]
+          averageColor.g += imageData[i + 1]
+          averageColor.b += imageData[i + 2]
+        }
 
-  const imageData = context.getImageData(0, 0, width, height).data
-  const length = imageData.length
+        const count = length / 4
+        averageColor.r = ~~(averageColor.r / count) // ~~ => convert to int
+        averageColor.g = ~~(averageColor.g / count)
+        averageColor.b = ~~(averageColor.b / count)
 
-  for (let i = 0; i < length; i += 4) {
-    averageColor.r += imageData[i]
-    averageColor.g += imageData[i + 1]
-    averageColor.b += imageData[i + 2]
-  }
-
-  const count = length / 4
-  averageColor.r = ~~(averageColor.r / count) // ~~ => convert to int
-  averageColor.g = ~~(averageColor.g / count)
-  averageColor.b = ~~(averageColor.b / count)
-
-  cache[imgElement.src] = averageColor
-
-  return averageColor
+        resolve(averageColor)
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,45 @@ import Link from "next/link"
 import CONFIG from "../../site.config"
 import { NextPageWithLayout } from "./_app"
 import { TypeAnimation } from "react-type-animation"
+import { getHueColorOfImage } from "../libs/utils/image"
+import { useEffect, useState } from "react"
 
 const IndexPage: NextPageWithLayout = () => {
+  const [bgColor, setBgColor] = useState<{ r: number; g: number; b: number }>({
+    r: 0,
+    g: 0,
+    b: 0,
+  })
+  useEffect(() => {
+    ;(async () => {
+      const hue = (await getHueColorOfImage(
+        "/music/grio-forward.jpg",
+        320,
+        320
+      )) as { r: number; g: number; b: number }
+
+      console.log(hue)
+      setBgColor(hue)
+    })()
+  }, [])
+
   return (
-    <div className="p-4 bg-white h-[85vh] dark:text-white dark:bg-zinc-700 rounded-md overflow-scroll"></div>
+    <div
+      className={`p-4 bg-white h-[85vh] rounded-md overflow-scroll`}
+      style={{
+        backgroundColor: `rgba(${bgColor.r}, ${bgColor.g}, ${bgColor.b}, 0.8)`,
+      }}
+    >
+      <div className="text-center">
+        <Image
+          className="rounded-md"
+          src="/music/grio-forward.jpg"
+          alt="앨범 커버"
+          width={"320px"}
+          height={"320px"}
+        />
+      </div>
+    </div>
   )
 }
 
