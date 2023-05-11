f0b8a59fe9add930525431b46b700b97907aa186
Bong <qhdgkdbs@gmail.com>
2023-05-11 23:10:18 +0900

    Style: 불필요 마진 삭제

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -22,7 +22,7 @@ const Layout: React.FC<Props> = ({
         <main
           className={`m-auto flex-grow w-full transition-all max-w-6xl px-4 xl:grid grid-cols-12 gap-6 ${
             fullWidth && "px-4 xl:px-24"
-          } ${metaConfig.type === "Paper" && "py-10"} `}
+          }`}
         >
           <div className="col-span-9">{children}</div>
           <div className="hidden xl:block xl:col-span-3">
