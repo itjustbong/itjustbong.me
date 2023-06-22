dc5b5b3cc9b1db0b1b781f38181e9a00038244ed
Bong <qhdgkdbs@gmail.com>
2023-06-22 17:01:36 +0900

    ✨ 홈 위젯 Chat draft 작업

---
+++ src/components/Widget/ChatBox.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled"
+
+const ChatBox = () => {
+  return <Container>GPT</Container>
+}
+
+export default ChatBox
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(122.53deg, #e0c3fc 0%, #8ec5fc 36.99%);
+  border-radius: 1rem;
+`
