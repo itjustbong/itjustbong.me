8a399fa6e379a06b91a14575b4a84a66c662174b
Bong <qhdgkdbs@gmail.com>
2023-06-22 17:01:55 +0900

    ✨ 홈 위젯 NameCard draft 작업

---
+++ src/components/Widget/NameCard.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled"
+
+const NameCard = () => {
+  return <Container>NameCard</Container>
+}
+
+export default NameCard
+
+const Container = styled.div`
+  background: rgba(255, 255, 255, 0.25);
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+`
