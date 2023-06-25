6402b06b87ff12d3bc32bc6523983964d88c2293
Bong <qhdgkdbs@gmail.com>
2023-06-25 19:12:19 +0900

    ✨ chat 타입에 따른 히스트로 기록

--- src/components/Widget/ChatBox.tsx
+++ src/components/Widget/ChatBox.tsx
@@ -1,39 +1,61 @@
 import styled from "@emotion/styled"
 import Image from "next/image"
-import { useCallback, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { AiOutlineSearch } from "react-icons/ai"
 import ChatBubble from "./ChatBox.Bubble"
 
 const ChatBox = () => {
-  const [query, setQuery] = useState<string>("")
-  const [response, setResponse] = useState<string>("")
+  const [history, setHistory] = useState<
+    {
+      type: "server" | "client"
+      message: string
+    }[]
+  >([])
+  const [currQuery, setCurrQuery] = useState<string>("")
+  const [currResponse, setCurrResponse] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
   const ref = useRef<HTMLDivElement>(null)
 
+  useEffect(() => {
+    if (currResponse === "") return
+    if (loading) return
+    setHistory((prev) => [...prev, { type: "server", message: currResponse }])
+  }, [currResponse, loading])
+
   const submitQuery = useCallback(async () => {
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: { "Content-Type": "text/event-stream" },
-      body: JSON.stringify({ message: query }),
-    })
-    const reader = (response.body as unknown as ReadableStream<Uint8Array>)
-      .pipeThrough(new TextDecoderStream())
-      .getReader()
-    while (true) {
-      const { value, done } = await reader.read()
-      if (done) break
-      const lines = value.toString().split("\n\n")
-      for (const line of lines) {
-        if (line.startsWith("data:")) {
-          const message = line.replace("data: ", "")
-          if (message === "undefined") break
-          setResponse((prev) => prev + message)
-          const $ = ref.current!
-          $.scrollTop = $.scrollHeight
+    setHistory((prev) => [...prev, { type: "client", message: currQuery }])
+    setLoading(true)
+
+    const $ = ref.current!
+    $.scrollTop = $.scrollHeight
+    try {
+      const response = await fetch("/api/chat", {
+        method: "POST",
+        headers: { "Content-Type": "text/event-stream" },
+        body: JSON.stringify({ message: currQuery }),
+      })
+      const reader = (response.body as unknown as ReadableStream<Uint8Array>)
+        .pipeThrough(new TextDecoderStream())
+        .getReader()
+      while (true) {
+        const { value, done } = await reader.read()
+        if (done) break
+        const lines = value.toString().split("\n\n")
+        for (const line of lines) {
+          if (line.startsWith("data:")) {
+            const message = line.replace("data: ", "")
+            if (message === "undefined") break
+            setCurrResponse((prev) => prev + message)
+            const $ = ref.current!
+            $.scrollTop = $.scrollHeight
+          }
         }
       }
-    }
-    setQuery("")
-  }, [query])
+      setLoading(false)
+      setCurrQuery("")
+      setCurrResponse("")
+    } catch (err) {}
+  }, [currQuery, setHistory])
 
   return (
     <Container>
@@ -59,15 +81,23 @@ const ChatBox = () => {
             <div className="h-5" />
             <div className="text-gray-500">저에 대해 궁금하신 것이 있나요?</div>
           </ChatBubble>
-          <ChatBubble type="client">{response}</ChatBubble>
+          {history.map((hist, idx) => (
+            <ChatBubble type={hist.type} key={idx}>
+              {hist.message}
+            </ChatBubble>
+          ))}
+          {currResponse && (
+            <ChatBubble type="server">{currResponse}</ChatBubble>
+          )}
         </div>
       </BubbleWrapper>
 
       <div className="flex gap-1 items-center absolute bottom-5 left-0 right-0 w-5/6 justify-evenly mx-auto">
         <input
           className="w-5/6 rounded-md h-10 border-none outline-none bg-[rgba(255,255,255,0.5)] focus:bg-white transition-all duration-300 px-2 text-gray-600 focus:text-gray-900"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          value={loading ? "답변을 기다리는 중..." : currQuery}
+          onChange={(e) => setCurrQuery(e.target.value)}
+          disabled={loading}
         />
         <AiOutlineSearch
           className="w-1/6 h-6 text-white"
