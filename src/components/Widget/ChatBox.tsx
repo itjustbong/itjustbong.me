2237dde7d7799e5b986599295b7dbf124f4d1fdb
Bong <qhdgkdbs@gmail.com>
2023-06-25 18:24:41 +0900

    💄 채팅바 하단 고정

--- src/components/Widget/ChatBox.tsx
+++ src/components/Widget/ChatBox.tsx
@@ -34,17 +34,33 @@ const ChatBox = () => {
 
   return (
     <Container>
-      <div className="flex items-center gap-4">
-        <Image
-          src="/images/chat-bubble.png"
-          height="48px"
-          width="48px"
-          alt="chat-bubble"
-        />
-        <div className="text-white">채팅으로 물어보세요</div>
-      </div>
+      <BubbleWrapper>
+        <div className="flex items-center gap-4 ">
+          <Image
+            src="/images/chat-bubble.png"
+            height="48px"
+            width="48px"
+            alt="chat-bubble"
+          />
+          <div className="text-white">채팅으로 물어보세요</div>
+        </div>
 
-      <div className="flex gap-1 items-center absolute bottom-5 w-5/6 justify-evenly right-0 left-0 mx-auto">
+        <div className="h-5" />
+
+        <div className="py-2">
+          <ChatBubble type="server">
+            <div className="font-bold">안녕하세요!</div>
+            <div className="font-bold text-gray-500">
+              프론트엔드 개발자, 봉승우입니다.
+            </div>
+            <div className="h-5" />
+            <div className="text-gray-500">저에 대해 궁금하신 것이 있나요?</div>
+          </ChatBubble>
+          <ChatBubble type="client">{response}</ChatBubble>
+        </div>
+      </BubbleWrapper>
+
+      <div className="flex gap-1 items-center absolute bottom-5 left-0 right-0 w-5/6 justify-evenly mx-auto">
         <input
           className="w-5/6 rounded-md h-10 border-none outline-none bg-[rgba(255,255,255,0.5)] focus:bg-white transition-all duration-300 px-2 text-gray-600 focus:text-gray-900"
           value={query}
@@ -55,25 +71,16 @@ const ChatBox = () => {
           onClick={submitQuery}
         />
       </div>
-
-      <div className="h-5" />
-
-      <div className="py-2">
-        <ChatBubble type="server">
-          <div className="font-bold">안녕하세요!</div>
-          <div className="font-bold text-gray-500">
-            프론트엔드 개발자, 봉승우입니다.
-          </div>
-          <div className="h-5" />
-          <div className="text-gray-500">저에 대해 궁금하신 것이 있나요?</div>
-        </ChatBubble>
-        <ChatBubble type="client">{response}</ChatBubble>
-      </div>
     </Container>
   )
 }
 
 export default ChatBox
+
+const BubbleWrapper = styled.div`
+  overflow: scroll;
+  height: calc(100% - 4rem);
+`
 
 const Container = styled.div`
   position: relative;
