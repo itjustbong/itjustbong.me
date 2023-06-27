19689e86c17952ed72869b88e50edd8867afbd14
Bong <qhdgkdbs@gmail.com>
2023-06-27 22:18:11 +0900

    ✨ chat API Stream 활용을 위한 SDK 활용

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,80 +1,40 @@
-import { IncomingMessage } from "http"
-import { NextApiRequest, NextApiResponse } from "next"
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from "openai-edge"
+import { OpenAIStream, StreamingTextResponse } from "ai"
 import { resumeData } from "./data"
-import { collection, addDoc } from "firebase/firestore/lite"
-import { db } from "./firebase"
 
-export const config = {
-  runtime: "edge",
-}
+const config = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+})
+const openai = new OpenAIApi(config)
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    await addDoc(collection(db, "itjustbong"), {
-      prompt: JSON.parse(req.body).message,
-    })
-  } catch (err) {
-    console.log(err)
-  }
+export const runtime = "edge"
+
+export default async function handler(req: Request) {
+  const { messages } = await req.json()
 
   try {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Cache-Control", "no-cache, no-transform")
-    res.setHeader("Content-Type", "text/event-stream;charset=utf-8")
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Connection", "keep-alive")
-    res.setHeader("X-Accel-Buffering", "no")
-    res.setHeader("Content-Encoding", "none")
-    res.flushHeaders()
-
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_KEY,
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-16k",
+      stream: true,
+      messages: [
+        { role: "system", content: resumeData },
+        {
+          role: "system",
+          content:
+            "봉승우와 관련된 답변을 해주면 돼. 봉승우와 관련없는 질문에 대해서는 답하지 않아도 돼. 그리고 너는 이제부터 봉승우야.",
+        },
+        {
+          role: "user",
+          content: `봉승우와 관련된 질문이야, ${
+            messages[messages.length - 1].content
+          }`,
+        },
+      ],
+      max_tokens: 500,
     })
-    const openai = new OpenAIApi(configuration)
 
-    const response = openai.createChatCompletion(
-      {
-        model: "gpt-3.5-turbo-16k",
-        messages: [
-          { role: "system", content: resumeData },
-          {
-            role: "system",
-            content:
-              "봉승우와 관련된 답변을 해주면 돼. 봉승우와 관련없는 질문에 대해서는 답하지 않아도 돼. 그리고 너는 이제부터 봉승우야.",
-          },
-          {
-            role: "user",
-            content:
-              "봉승우와 관련된 질문이야, " + JSON.parse(req.body).message,
-          },
-        ],
-        max_tokens: 500,
-        stream: true,
-      },
-      { responseType: "stream" }
-    )
-
-    response.then((resp) => {
-      ;(resp.data as unknown as IncomingMessage).on("data", (data: string) => {
-        const lines = data.toString().split("\n\n")
-
-        for (const line of lines) {
-          if (line === "data: [DONE]") {
-            res.end()
-            return
-          }
-          if (line.startsWith("data:")) {
-            const message = line.replace("data: ", "")
-            const parsed = JSON.parse(message)
-            res.write(`data: ${parsed.choices[0].delta.content}\n\n`)
-          }
-        }
-      })
-    })
+    const stream = OpenAIStream(response)
+    return new StreamingTextResponse(stream)
   } catch (err) {
     console.log(err)
   }
