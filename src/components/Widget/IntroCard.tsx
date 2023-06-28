cd678fbd901ab683d0f1cc84a1e661355127ac69
Bong <qhdgkdbs@gmail.com>
2023-06-28 20:17:45 +0900

    💄 타이틀 세팅

--- src/components/Widget/IntroCard.tsx
+++ src/components/Widget/IntroCard.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/css"
 import styled from "@emotion/styled"
 
 const 문구 = [
@@ -13,10 +12,11 @@ const 문구 = [
 
 const IntroCard = () => {
   return (
-    <Container className="py-1 px-2 sm:px-4">
-      <div className="font-bold text">
+    <Container className="py-1 px-2 sm:px-4 overflow-hidden">
+      <Title>History</Title>
+      <ScorllTitle className="hover:-translate-x-16">
         도전적이며 자기주도적인 삶을 지향합니다.
-      </div>
+      </ScorllTitle>
       <div className="text-gray-500">문구[]</div>
     </Container>
   )
@@ -24,7 +24,27 @@ const IntroCard = () => {
 
 export default IntroCard
 
+const Title = styled.div`
+  position: absolute;
+  top: 1rem;
+  font-weight: bold;
+  background: -webkit-linear-gradient(#121fcf, #cf1512);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const ScorllTitle = styled.div`
+  cursor: pointer;
+  position: absolute;
+  top: 3rem;
+  font-weight: bold;
+  width: 30rem;
+  transition: all ease 0.5s;
+  animation: auto-scroll-x 2.5s ease 1;
+`
+
 const Container = styled.div`
+  position: relative;
   background: rgba(255, 255, 255, 0.5);
   width: 100%;
   height: 100%;
