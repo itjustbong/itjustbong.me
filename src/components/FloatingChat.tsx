3fa183e1e5bb9ad97eb0b1d98b505d617b418cd8
Bong <qhdgkdbs@gmail.com>
2023-06-28 17:05:58 +0900

    ✨ chatBox 토글 기능 추가

--- src/components/FloatingChat.tsx
+++ src/components/FloatingChat.tsx
@@ -1,15 +1,18 @@
 import styled from "@emotion/styled"
 import Image from "next/image"
-import { useEffect, useRef } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+import ChatBox from "./Widget/ChatBox"
+import { AiFillCloseCircle } from "react-icons/ai"
 
 const FloatingChat = () => {
+  const [chatOpen, setChatOpen] = useState<boolean>(false)
   const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const shrinkTimeout = setTimeout(() => {
       if (!ref.current) return
       const $ = ref.current
-      $.style.width = "3.5rem"
+      $.style.width = "3.45rem"
     }, 2500)
 
     return () => {
@@ -17,27 +20,68 @@ const FloatingChat = () => {
     }
   }, [])
 
+  const handleChatToggle = useCallback(() => {
+    if (!chatOpen) setChatOpen(true)
+    else setChatOpen(false)
+  }, [chatOpen])
+
   return (
     <Container
       ref={ref}
       className="shadow-lg transition-all duration-300 overflow-hidden w-[13.5rem] hover:!w-[13.5rem]"
     >
-      <Image
-        src={"/images/chat-bubble.png"}
-        width={24}
-        height={24}
-        alt="chat"
-      />
-      <div className="flex-shrink-0">Chat으로 물어보세요!</div>
+      <FloatingElement onClick={handleChatToggle}>
+        <Image
+          src={"/images/chat-bubble.png"}
+          width={24}
+          height={24}
+          alt="chat"
+        />
+        <div className="flex-shrink-0">Chat으로 물어보세요!</div>
+      </FloatingElement>
+
+      <ChatContainer
+        className={`shadow-lg ${chatOpen ? "scale-100" : "scale-0"}`}
+      >
+        <ChatCloseBtn onClick={handleChatToggle}>
+          <AiFillCloseCircle size={32} />
+        </ChatCloseBtn>
+        <ChatBox />
+      </ChatContainer>
     </Container>
   )
 }
 
 export default FloatingChat
 
-const Container = styled.div`
+const ChatCloseBtn = styled.div`
+  position: absolute;
+  z-index: 12;
+  color: white;
+  top: 1rem;
+  right: 1rem;
+  size: 4rem;
+`
+
+const ChatContainer = styled.div`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  min-width: 340px;
+  width: 30vw;
+  max-width: 480px;
+  height: 480px;
+  border-radius: 1rem;
+  transition: all ease 0.5s;
+  transform-origin: 95% 100%;
+`
+
+const FloatingElement = styled.div`
   display: flex;
   gap: 1rem;
+`
+
+const Container = styled.div`
   position: fixed;
   bottom: 1rem;
   right: 1rem;
