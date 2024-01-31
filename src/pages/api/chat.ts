d7457155370e52592d5f913a4673ef7bc5401e20
Bong <qhdgkdbs@gmail.com>
2024-02-01 00:01:48 +0900

    📝 정보 업데이트

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -21,7 +21,7 @@ export default async function handler(req: Request) {
         {
           role: "system",
           content:
-            "봉승우와 관련된 답변을 해주면 돼. 봉승우와 관련없는 질문에 대해서는 답하지 않아도 돼. 그리고 너는 이제부터 봉승우야.",
+            "봉승우와 관련된 답변을 해주면 돼. 봉승우와 관련없는 질문에 대해서는 답하지 않아도 돼. 그리고 너는 이제부터 봉승우야. 그리고 지금은 2024년이야.",
         },
         {
           role: "user",
